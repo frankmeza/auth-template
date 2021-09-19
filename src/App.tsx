ca29f4746908bc39ff2ff4cf2a94b73b006e80e6
Frank Meza <frankmezapdx@gmail.com>
2021-09-18 22:29:27 -0700

    rm keyPad, change to counter app example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { useState } from "../public";
 
 import "./app.scss";
-import { Keypad } from "./keypad";
 
 const App = (): JSX.Element => {
-    return <Keypad />;
+    const [counterState, setCounterState] = useState(0);
+
+    const increment = () => {
+        setCounterState(counterState + 1);
+    };
+
+    const decrement = () => {
+        setCounterState(counterState - 1);
+    };
+
+    return (
+        <React.Fragment>
+            <h2 onClick={increment}>Add 1</h2>
+            <p>{counterState}</p>
+            <h2 onClick={decrement}>Minus 1</h2>
+        </React.Fragment>
+    );
 };
 
 export default App;
